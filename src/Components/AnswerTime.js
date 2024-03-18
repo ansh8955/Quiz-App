ba41c8b922910530c7230f13d7bc3a8569751628
Ansh Upadhyay <upadhyayaansh094@gmail.com>
2024-03-18 22:20:40 +0530

    first commit

---
+++ src/Components/AnswerTime.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AnswerTimer.css';
+
+
+
+function AnswerTimer(){
+
+return
+(
+
+<div className='answer-timer-container'>
+
+<div className='progress'>
+
+<h1>Hello</h1>
+</div>
+</div>
+
+);
+
+}
+
+
+export default AnswerTimer;
